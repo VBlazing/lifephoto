2e8fe3514db00eefff55441ffa63a489739e7804
VBlazing <18392192892@163.com>
2025-11-18 10:08:13 +0800

    feat: add proxy

--- proxy.ts
+++ proxy.ts
@@ -2,15 +2,18 @@
  * @Author: VBlazing
  * @Date: 2025-11-06 22:32:53
  * @LastEditors: VBlazing
- * @LastEditTime: 2025-11-06 23:02:07
+ * @LastEditTime: 2025-11-18 10:08:02
  * @Description: proxy 代理
  */
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
-  console.log('request', request)
+  const { pathname } = request.nextUrl
+  if (pathname.endsWith('/')) {
+    return NextResponse.redirect(new URL('/beauty', request.url))
+  }
 }
 
 // See "Matching Paths" below to learn more
